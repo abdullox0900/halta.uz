460eb7c040856b2b0bf06d76524e940ad3a784b5
abdullox0900 <abdullox0900@gmail.com>
2023-11-20 23:48:25 +0500

    fix: style file

--- components/Card/Style.tsx
+++ components/Card/Style.tsx
@@ -23,7 +23,7 @@ export const CardItem = styled.li`
 
 
 
-    
+
     overflow: hidden;
     background-color: white;
     /* box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px; */
@@ -51,6 +51,9 @@ export const CardItem = styled.li`
 export const CardItemImg = styled.img`
     width: 250px;
     height: 200px;
+
+
+    
     object-fit: cover;
 
 
